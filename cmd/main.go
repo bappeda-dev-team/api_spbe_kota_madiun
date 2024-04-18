7b82a4f4af80e0d74d249d73d228236e4621af03
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-04-18 13:30:35 +0700

    spacing jadi 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-        "encoding/json"
-        "log"
-        "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 func main() {
-        addr := ":3001"
-        mux := http.NewServeMux()
+	addr := ":3001"
+	mux := http.NewServeMux()
 
-        mux.HandleFunc("/",
-        func(w http.ResponseWriter, r *http.Request) {
-                enc := json.NewEncoder(w)
-                w.Header().
-                Set("Content-Type",
-                "application/json; charset=utf-8")
+	mux.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			enc := json.NewEncoder(w)
+			w.Header().
+				Set("Content-Type",
+					"application/json; charset=utf-8")
 
-                resp := Resp{
-                        Message: "SPBE KOTA MADIUN",
-                }
+			resp := Resp{
+				Message: "SPBE KOTA MADIUN",
+			}
 
-                if err := enc.Encode(resp); err != nil {
-                        panic(err)
-                }
-        })
+			if err := enc.Encode(resp); err != nil {
+				panic(err)
+			}
+		})
 
-        log.Printf("listening on %s\n", addr)
+	log.Printf("listening on %s\n", addr)
 
-        log.Fatal(http.ListenAndServe(addr, mux))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
 
 type Resp struct {
-        Message string `json:"message"`
+	Message string `json:"message"`
 }
-
