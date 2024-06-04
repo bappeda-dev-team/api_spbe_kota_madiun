06956d0902c9ce4099ba4940c83cf2b56c1825f5
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-06-04 10:02:01 +0700

    dockerize app

--- app/database.go
+++ app/database.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -18,11 +17,14 @@ func GetConnection() *sql.DB {
 		// log.Fatal("Error loading .env file")
 	}
 
-	dbUser := os.Getenv("DB_USER")
-	dbPort := os.Getenv("DB_PORT") // Ensure this is set to something like "3306"
-	dbName := os.Getenv("DB_NAME")
+	// !! diganti dengan DB_URL agar bisa konek di docker-compose
+	// contoh: "root@tcp(localhost:3306)/db_spbe?parseTime=true"
+	// dbUser := os.Getenv("DB_USER")
+	// dbPort := os.Getenv("DB_PORT") // Ensure this is set to something like "3306"
+	// dbName := os.Getenv("DB_NAME")
 
-	connStr := fmt.Sprintf("%s@tcp(localhost:%s)/%s?parseTime=true", dbUser, dbPort, dbName)
+	// connStr := fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?parseTime=true", dbUser, dbPass, dbPort, dbName)
+	connStr := os.Getenv("DB_URL")
 
 	db, err := sql.Open("mysql", connStr)
 	if err != nil {
--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"api_spbe_kota_madiun/service"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/rs/cors"
@@ -23,7 +24,7 @@ func main() {
 	referensiarsitekturController := controller.NewReferensiarstitekturController(referesiarsitekturService)
 
 	prosesbisnisRepository := repository.NewProsesBisnisRepository()
-	prosesbisnisService  := service.NewProsesBisnisService(referensiarsitekturRepository, prosesbisnisRepository, db, validate)
+	prosesbisnisService := service.NewProsesBisnisService(referensiarsitekturRepository, prosesbisnisRepository, db, validate)
 	prosesbisnisController := controller.NewProsesBisnisController(prosesbisnisService)
 
 	router := app.NewRouter(referensiarsitekturController, prosesbisnisController)
@@ -35,9 +36,20 @@ func main() {
 	})
 
 	handler := c.Handler(router)
+	// ambil host dan port di env
+	// agar bisa disetting diluar build
+	// localhost tidak bisa dipakai untuk production
+	host := os.Getenv("host")
+	port := os.Getenv("port")
+	addr := fmt.Sprintf("%s:%s", host, port)
+
+	// set default value host and port
+	if addr == ":" {
+		addr = "localhost:8080"
+	}
 
 	server := http.Server{
-		Addr:    "localhost:8080",
+		Addr:    addr,
 		Handler: handler,
 	}
 
