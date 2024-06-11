a7023c81774917b3d8a784be98fe0be0a267be68
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2024-06-11 16:11:32 +0700

    update get all pohon kinerja & sasaran kota

--- app/database.go
+++ app/database.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -19,12 +20,12 @@ func GetConnection() *sql.DB {
 
 	// !! diganti dengan DB_URL agar bisa konek di docker-compose
 	// contoh: "root@tcp(localhost:3306)/db_spbe?parseTime=true"
-	// dbUser := os.Getenv("DB_USER")
-	// dbPort := os.Getenv("DB_PORT") // Ensure this is set to something like "3306"
-	// dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPort := os.Getenv("DB_PORT") // Ensure this is set to something like "3306"
+	dbName := os.Getenv("DB_NAME")
 
-	// connStr := fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?parseTime=true", dbUser, dbPass, dbPort, dbName)
-	connStr := os.Getenv("DB_URL")
+	connStr := fmt.Sprintf("%s@tcp(localhost:%s)/%s?parseTime=true", dbUser, dbPort, dbName)
+	// connStr := os.Getenv("DB_URL")
 
 	db, err := sql.Open("mysql", connStr)
 	if err != nil {
--- app/database_test.go
+++ app/database_test.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"database/sql"
-	"fmt"
 	"os"
 	"testing"
 	"time"
