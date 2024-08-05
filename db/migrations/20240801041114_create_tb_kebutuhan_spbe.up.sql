d2687973a667e33a75386fd03e051d9b6c4d0a7a
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-08-05 19:16:09 +0700

    not null fix

--- db/migrations/20240801041114_create_tb_kebutuhan_spbe.up.sql
+++ db/migrations/20240801041114_create_tb_kebutuhan_spbe.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE kebutuhan_spbe
 (
     id INT NOT NULL AUTO_INCREMENT,
-    nama_domain VARCHAR(255) NOT NULL,
+    nama_domain VARCHAR(255),
     kode_opd VARCHAR(255),
     tahun INT NOT NULL,
     id_prosesbisnis INT NOT NULL,
@@ -9,6 +9,4 @@ CREATE TABLE kebutuhan_spbe
     CONSTRAINT fk_domain FOREIGN KEY (nama_domain) REFERENCES domain_spbe (nama_domain),
     CONSTRAINT fk_kebutuhan_spbe_kode_opd FOREIGN KEY (kode_opd) REFERENCES opd (kode_opd),
     CONSTRAINT fk_kebutuhan_spbe_id_prosesbisnis FOREIGN KEY (id_prosesbisnis) REFERENCES proses_bisnis (id)
-) ENGINE=InnoDB
-    DEFAULT CHARSET=latin1
-    COLLATE latin1_general_ci;
+) ENGINE=InnoDB;
