2d2a4d86837cd6cbd8bbc76c6e9f0bc7a8355934
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2024-08-26 01:46:59 +0700

    fix keterangan request

--- model/web/kebutuhanspbe_update_request.go
+++ model/web/kebutuhanspbe_update_request.go
@@ -26,7 +26,7 @@ type KondisiAwalUpdate struct {
 
 type KebutuhanSPBEKeteranganUpdateRequest struct {
 	ID         int    `json:"id"`
-	Keterangan string `json:"keterangan"`
+	Keterangan string `json:"keterangan_gap"`
 }
 
 type KebutuhanSPBEPjUpdateRequest struct {
